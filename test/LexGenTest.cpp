8e9034e5d3905bff1981dd383fc0f6d21fab8712
hongwozai <hongwozai@163.com>
2018-08-11 10:33:51 +0000

    简单初始化

---
+++ test/LexGenTest.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "NFA.h"
+#include "DFA.h"
+#include "LexGen.h"
+using namespace std;
+
+enum {
+    START = 0,
+    END,
+    TAG,
+    WORD,
+
+    TAG_INTERNAL,
+};
+
+TEST(lexgen, a)
+{
+    int ret;
+    LexGen::res res;
+    LexGen lexgen;
+
+    LexGen::conf array[] = {
+        {0, 1, "\\w+", true},
+        {0, 2, "<[^>]+>", true}
+    };
+
+    lexgen.init(array, 2);
+
+    string html = "word<html>";
+    ret = lexgen.match(html.data(), html.size(), &res);
+
+    if (ret == -1) {
+        cout << "match failure" << endl;
+    } else {
+        cout << "res.type: " << res.type << endl;
+        cout << "res.size: " << res.size << endl;
+        cout << string(html.data(), res.size) << endl;
+    }
+}
